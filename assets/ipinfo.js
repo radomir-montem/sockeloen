6ae47f0bf565e8572f72b5edfe23925b4c71ff18
radomir-montem <radomir@montemagency.com>
2025-07-10 15:50:08 +0900

    disable redirect tool

--- assets/ipinfo.js
+++ assets/ipinfo.js
@@ -1,25 +1,25 @@
-if (!navigator.userAgent.match(/bot|spider/i)) {
-    //Let's check if we have the value in sessionStorage
-    if (!sessionStorage.getItem('visitor_country')) {
-        $.get("https://ipinfo.io", function(response) {
-            console.log(response.city, response.country);
-            sessionStorage.setItem('visitor_country', response.country);
-            const form = document.getElementById('HeaderLocalizationForm');
-            const country_code = response.country;
-            if(country_code == 'NL' || country_code == 'BE') {
-                form.querySelector('[name="country_code"]').value = 'NL';
-                form.querySelector('[name="locale_code"]').value = 'nl';
-            } else if (country_code == 'DE' || country_code == 'AT') {
-                form.querySelector('[name="country_code"]').value = 'DE';
-                form.querySelector('[name="locale_code"]').value = 'de';
-            } else if (country_code == 'US' || country_code == 'CA') {
-                form.querySelector('[name="country_code"]').value = 'US';
-                form.querySelector('[name="locale_code"]').value = 'en';
-            } else {
-                form.querySelector('[name="country_code"]').value = 'NL';
-                form.querySelector('[name="locale_code"]').value = 'en';
-            }
-            form.submit();
-        }, "jsonp");
-    }
-}
+// if (!navigator.userAgent.match(/bot|spider/i)) {
+//     //Let's check if we have the value in sessionStorage
+//     if (!sessionStorage.getItem('visitor_country')) {
+//         $.get("https://ipinfo.io", function(response) {
+//             console.log(response.city, response.country);
+//             sessionStorage.setItem('visitor_country', response.country);
+//             const form = document.getElementById('HeaderLocalizationForm');
+//             const country_code = response.country;
+//             if(country_code == 'NL' || country_code == 'BE') {
+//                 form.querySelector('[name="country_code"]').value = 'NL';
+//                 form.querySelector('[name="locale_code"]').value = 'nl';
+//             } else if (country_code == 'DE' || country_code == 'AT') {
+//                 form.querySelector('[name="country_code"]').value = 'DE';
+//                 form.querySelector('[name="locale_code"]').value = 'de';
+//             } else if (country_code == 'US' || country_code == 'CA') {
+//                 form.querySelector('[name="country_code"]').value = 'US';
+//                 form.querySelector('[name="locale_code"]').value = 'en';
+//             } else {
+//                 form.querySelector('[name="country_code"]').value = 'NL';
+//                 form.querySelector('[name="locale_code"]').value = 'en';
+//             }
+//             form.submit();
+//         }, "jsonp");
+//     }
+// }
